bff33bbc8dfc88318d71231c0968aa2f8fe66cf9
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-09-18 17:36:24 -0400

    UI: Add calc button layout, assign operation func

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ function parseCalcInput (strInput) {
 }
 
 // operations
+function operate (operator, op1, op2) {
+    switch (operator) {
+        case '+':
+            return add(op1, op2);
+        case '-':
+            return sub(op1, op2);
+        case '*':
+            return multiply(op1, op2);
+        case '/':
+            return divide(op1, op2);
+        default:
+            return 'ERROR';
+    }
+}                                                                                                           
+
 function add (op1, op2) {
     return op1 + op2;
 }
