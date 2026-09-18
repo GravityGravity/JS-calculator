659fd0694a31c772646941e620bc0997caf6b1d0
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-09-18 16:09:46 -0400

    init commit, basic application file layout

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// === FUNCTIONS ===
+function parseCalcInput (strInput) {
+}
+
+// operations
+function add (op1, op2) {
+    return op1 + op2;
+}
+
+function sub (op1, op2) {
+    return op1 - op2;
+}
+
+function multiply (op1, op2) {
+    return op1 * op2;
+}
+
+function divide (op1, op2) {
+    return op1 / op2;
+}
